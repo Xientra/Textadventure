2d982e93289b9f7008ee16312784e96df98e1653
JadaMaar <42937194+JadaMaar@users.noreply.github.com>
2018-09-04 18:14:29 +0200

    Revert "Hat jetzt tazächlich ein dynamisches 3D Array"
    This reverts commit 58256fc63bf4f923a2d3c0113dd13bc946dbfac1.

--- mainunit.pas
+++ mainunit.pas
@@ -5,15 +5,13 @@ unit MainUnit;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  TRaum;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, TRaum;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Memo1: TMemo;
     procedure FormCreate(Sender: TObject);
   private
 
@@ -23,34 +21,20 @@ type
 
 var
   Form1: TForm1;
-  Room: Array of Array of Array of TRoom;
-  Room_x, Room_y, Room_z: integer;
+  //x,y,z: integer;
+  Room: Array[0..5,0..5,0..5] of TRoom;
 implementation
 
 {$R *.lfm}
 
+//Hi?
+
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  i, ii: integer;
 begin
-  Room_x := 5;
-  Room_y := 5;
-  Room_z := 5;
 
-  SetLength(Room, Room_x);
-  for i := 0 to Room_y - 1 do
-  begin
-    SetLength(Room[i], Room_y);
-    for ii := 0 to Room_z - 1 do
-      SetLength(Room[i, ii], Room_z);
-  end;
 end;
 
 end.
-
-
-
-
 
