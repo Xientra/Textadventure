07225df2b08ac6dcbd9fa564b8fa92855130eda7
Xientra <38855570+Xientra@users.noreply.github.com>
2018-09-04 18:33:37 +0200

    bitte sag mir es gibt ein 3d array...
    Ich hab was getan...

--- unit1.pas
+++ unit1.pas
@@ -21,7 +21,8 @@ type
 
 var
   Form1: TForm1;
-
+  Room: Array of Array of Array of TRoom;
+  Room_x, Room_y, Room_z: integer;
 implementation
 
 {$R *.lfm}
@@ -29,8 +30,20 @@ implementation
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  i, ii: integer;
 begin
 
+  Room_x := 5;
+  Room_y := 5;
+  Romm_z := 5;
+  SetLength(Room, Room_x);
+  for i := 0 to Room_y - 1 do
+  begin
+    SetLength(Room[i], Room_y);
+    for ii := 0 to Room_z - 1 do
+      SetLength(Room[i, ii], Room_z);
+  end;
 end;
 
 end.
