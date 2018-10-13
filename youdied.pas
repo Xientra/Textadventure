9d953ac910393b59d4a0e597434dfea50502d3c2
Xientra <38855570+Xientra@users.noreply.github.com>
2018-10-13 21:07:52 +0200

    Added the remaining Keys and the MagicDagger; All the Menüs sehen nun gut aus

--- youdied.pas
+++ youdied.pas
@@ -13,12 +13,22 @@ type
   { TForm3 }
 
   TForm3 = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
+    Image1: TImage;
+    Image2: TImage;
+    Header_Label: TLabel;
     Label1: TLabel;
+    Label2: TLabel;
     Timer1: TTimer;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
+    procedure Image2Click(Sender: TObject);
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure Image2MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure Image2MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure Image1MouseEnter(Sender: TObject);
+    procedure Image1MouseLeave(Sender: TObject);
+    procedure Image2MouseEnter(Sender: TObject);
+    procedure Image2MouseLeave(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
@@ -36,17 +46,7 @@ uses unit1, menue;
 
 { TForm3 }
 
-procedure TForm3.Button1Click(Sender: TObject);
-begin
-  Form1.visible := false;
-  Form2.visible := true;
-  Form3.visible := false;
-end;
 
-procedure TForm3.Button2Click(Sender: TObject);
-begin
-  Form2.close;
-end;
 
 procedure TForm3.Timer1Timer(Sender: TObject);
 begin
@@ -57,5 +57,27 @@ begin
     Timer1.Enabled := false;
   end;
 end;
+
+procedure TForm3.Image1Click(Sender: TObject);
+begin
+  Form1.visible := false;
+  Form2.visible := true;
+  Form3.visible := false;
+end;
+
+procedure TForm3.Image2Click(Sender: TObject);
+begin
+  Form2.close;
+end;
+
+procedure TForm3.Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer); begin Image1.Picture.LoadFromFile('Images/Buttons/Button_down.png'); end;
+procedure TForm3.Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer); begin Image1.Picture.LoadFromFile('Images/Buttons/Button.png'); end;
+procedure TForm3.Image2MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer); begin Image2.Picture.LoadFromFile('Images/Buttons/Button_down.png'); end;
+procedure TForm3.Image2MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer); begin Image2.Picture.LoadFromFile('Images/Buttons/Button.png'); end;
+
+procedure TForm3.Image1MouseEnter(Sender: TObject); begin Image1.Picture.LoadFromFile('Images/Buttons/Button_hover.png'); end;
+procedure TForm3.Image1MouseLeave(Sender: TObject); begin Image1.Picture.LoadFromFile('Images/Buttons/Button_down.png'); end;
+procedure TForm3.Image2MouseEnter(Sender: TObject); begin Image2.Picture.LoadFromFile('Images/Buttons/Button_hover.png'); end;
+procedure TForm3.Image2MouseLeave(Sender: TObject); begin Image2.Picture.LoadFromFile('Images/Buttons/Button_down.png'); end;
 
 end.
